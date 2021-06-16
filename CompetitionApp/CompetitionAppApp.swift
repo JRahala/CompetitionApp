b7c5ff39042dcb2cd7cb6239d75c425d6cd551ba
Jasamrit Rahala <rahala.j@etoncollege.org.uk>
2021-06-16 22:04:38 +0100

    Initial Commit

---
+++ CompetitionApp/CompetitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompetitionAppApp.swift
+//  CompetitionApp
+//
+//  Created by Rahala, Jasamrit (Coll) on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CompetitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
