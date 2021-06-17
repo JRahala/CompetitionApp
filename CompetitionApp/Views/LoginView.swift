7aad64ae6f65ad380f7b4568640f186ea9c3c683
Jasamrit Rahala <rahala.j@etoncollege.org.uk>
2021-06-17 20:48:27 +0100

    Login Screen Functional

--- CompetitionApp/Views/LoginView.swift
+++ CompetitionApp/Views/LoginView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var result: String = ""
+    @State var currentUser: Person? = nil
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(entity: Person.entity(), sortDescriptors: []) var people: FetchedResults<Person>
+    
     var body: some View {
-        Text("Login View")
-            NavigationLink(
-                destination: LoginView(), //MenuView(),
-                label:{
-                    Text("Menu View")
+        VStack{
+            
+            Text("Login View")
+            
+            TextField("Username", text: $username)
+            SecureField("Password", text: $password)
+            
+            Button(action: {
+                result = loginUser()
+            }, label: {
+                Text("Login")
             })
+            
+            if result != "" {
+                Text("Result: \(result)")
+                
+            }
+            
+            if result == "Login successful!"{
+                NavigationLink(destination: {
+                    MenuView(currentUser: currentUser!)
+                }(), label: {
+                    Text("Continue to menu screen")
+                })
+            }
+        }
+        .padding()
     }
+    
+    func loginUser() -> String {
+        for person in people{
+            if (person.username! == self.username){
+                if (person.password == self.password){
+                    currentUser = person
+                    return "Login successful!"
+                }
+                else{
+                    return "Incorrect password"
+                }
+            }
+        }
+        return "Incorrect username"
+    }
+    
 }
 
 struct LoginView_Previews: PreviewProvider {
