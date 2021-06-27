daf6aa45ef90dbcded50151c41e5490ece932264
Jasamrit Rahala <rahala.j@etoncollege.org.uk>
2021-06-27 18:58:26 +0100

    added alerts after testing

--- CompetitionApp/Views/TestView.swift
+++ CompetitionApp/Views/TestView.swift
@@ -14,21 +14,31 @@ struct TestView: View {
     var difficultyLevel: Int64 {
         currentUser.experience / 100 + 3
     }
+    @State var questionNumber: Int = 0
+    @State var correct: WordResponse? = nil
         
     var body: some View{
         NavigationView{
             VStack{
-                if (testWords).count == 4{
+                if (testWords).count == 4 && questionNumber < 2{
+                    questionNumber += 1
+                    Text("Definition")
+                    let correct = testWords.randomElement()!
+                    Text(correct.meanings?[0].definitions?[0].definition ?? "")
+                    
                     VStack{
                         ForEach(0..<4, id: \.self){ index in
-                            Text(testWords[index].word!)
+                            Button(action: {
+                                wordPressed(choice: testWords[index])
+                            }, label: {Text(testWords[index].word!)})
+                            
                         }
                     }
                 }
             }
             .onAppear(perform: {
                 testWords = []
-                for i in 0..<2{
+                for _ in 0..<2{
                     (requestWord(level: difficultyLevel, callback: {data in
                         print("words retrieved", data.word)
                         requestDefinition(word: data.word, callback: {def in
@@ -38,6 +48,29 @@ struct TestView: View {
                     }))
                 }
             })
+        }
+    }
+    
+    func wordPressed(choice: WordResponse){
+        var alertTitle = ""
+        if choice == correct{
+            alertTitle = "Well done"
+        }
+        else{
+            alertTitle = "Unlucky"
+        }
+        
+        let alertMessage = "The correct answer was \(correct?.word!)"
+        Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("continue")))
+        testWords = []
+        for _ in 0..<2{
+            (requestWord(level: difficultyLevel, callback: {data in
+                print("words retrieved", data.word)
+                requestDefinition(word: data.word, callback: {def in
+                    testWords.append(def)
+                    //print("definition retrieved", testWords)
+                })
+            }))
         }
     }
     
